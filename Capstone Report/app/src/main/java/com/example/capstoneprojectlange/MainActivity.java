c306637f30e6dadf00b5604bd069114330b3abad
CanielDarkk <02daniel.clark@gulllakecs.org>
2021-06-01 10:40:46 -0400

    Added Android Studio and Edit Text

---
+++ Capstone Report/app/src/main/java/com/example/capstoneprojectlange/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.capstoneprojectlange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
